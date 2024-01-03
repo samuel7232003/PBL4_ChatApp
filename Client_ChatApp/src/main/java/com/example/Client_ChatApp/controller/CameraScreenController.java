49599e6ccb5694624860f929b6b78ec0f63c2242
Thanh <thanhleviet723@gmail.com>
2024-01-04 05:35:48 +0700

    thanh

--- Client_ChatApp/src/main/java/com/example/Client_ChatApp/controller/CameraScreenController.java
+++ Client_ChatApp/src/main/java/com/example/Client_ChatApp/controller/CameraScreenController.java
@@ -3,11 +3,12 @@ package com.example.Client_ChatApp.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 public class CameraScreenController {
     @FXML
-    private Label cameraScreen;
+    private ImageView cameraScreen;
     @FXML
     private Button take;
 
